d6c8c42c163faa6c89347de575df62d0ad7a6d7a
fikri <fikri_jwahid@yahoo.com>
2019-11-13 16:33:42 +0800

    first commit

---
+++ dnm/js/test.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//     $("div").click(function() {alert("Hello, world!");}
+//     );
+// });
+
+
+document.onmouseup = () => {
+
+    console.log(window.getSelection().toString());
+};
+
